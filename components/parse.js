45893e578fb237f951e402bd86bc1ad97fa5605f
nishantb1 <bobbane1011@gmail.com>
2024-01-28 02:13:13 -0600

    leaderbord

---
+++ components/parse.js
@@ -0,0 +1,11 @@
+import RNFS from 'react-native-fs';
+
+const path = RNFS.DocumentDirectoryPath + 'components\users.json';
+
+RNFS.readFile(path, 'utf8')
+    .then(contents => {
+        const data = JSON.parse(contents);
+    })
+    .catch(err => {
+        console.log('Failed to read users.json', err);
+    });
